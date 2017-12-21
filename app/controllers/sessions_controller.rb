c9cc18e0966bf56740ebba75da4b9e3e0414ca9b
Lita Stephenson <Lita.Stephenson@gmail.com>
2017-12-21 14:32:42 -0800

    built auth flow

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,17 @@
 class SessionsController < ApplicationController
   def create
-    @user = User.find_or_create_from_auth_hash(auth_hash)
-    self.current_user = @user
-    redirect_to '/'
+    @user = User.find_or_create_by(instagram_id: auth['uid']) do |u|
+      u.username = auth['info']['nickname']
+      u.profile_picture = auth['info']['image']
+    end
+    session[:user_id] = @user.id
+    session[:access_token] = auth['credentials']['token']
+    redirect_to root_path
   end
 
   protected
 
-  def auth_hash
+  def auth
     request.env['omniauth.auth']
   end
 end
