fbb530f175066fb7df15c65a453998370cfe1820
Lita Stephenson <Lita.Stephenson@gmail.com>
2017-12-21 16:39:22 -0800

    moved reactDOM render to application, split out app component

--- app/javascript/packs/components/app.jsx
+++ app/javascript/packs/components/app.jsx
@@ -1,28 +1,23 @@
-//     <a href='https://api.instagram.com/oauth/authorize/?client_id=afb682c734104e9790fe86ed286db918&redirect_uri=http://localhost:3000/auth/instagram/callback&response_type=code'>login</a>
-
-
 import React from 'react'
-import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 
-class App extends React.Component {
+import Header from './header'
+import Footer from './footer'
+
+export default class App extends React.Component {
   
   render() {
     return (
       <div>
-        {/* <Header /> */}
+        <Header isLoggedIn={false}/>
         <div className="container">
+          Hello
           {/* <Primary /> */}
+          <Footer />
         </div>
-        {/* <Footer /> */}
       </div>
     )
   }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(
-    <App />,
-    document.body
-  )
-})
+
