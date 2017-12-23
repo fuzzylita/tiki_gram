989a884aeca5f0efb9631399f7e697016f40c5f6
Lita Stephenson <Lita.Stephenson@gmail.com>
2017-12-23 15:18:23 -0800

    set up user login/logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class SessionsController < ApplicationController
-  skip_before_action :require_login, only: [:create]
+  skip_before_action :require_login, only: [:create, :login]
 
   def create
     @user = User.find_or_create_by(instagram_id: auth['uid']) do |u|
@@ -8,6 +8,16 @@ class SessionsController < ApplicationController
     end
     session[:user_id] = @user.id
     session[:access_token] = auth['credentials']['token']
+    redirect_to root_path
+  end
+
+  def login
+    login_path = "https://api.instagram.com/oauth/authorize/?client_id=#{ENV['INSTAGRAM_ID']}&redirect_uri=#{ENV['REDIRECT_HOST']}/auth/instagram/callback&response_type=code"
+    redirect_to login_path
+  end
+
+  def logout
+    session.clear
     redirect_to root_path
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   
   get '/auth/:provider/callback', to: 'sessions#create'
   get '/user', to: 'users#show'
+  get '/login', to: 'sessions#login'
+  get '/logout', to: 'sessions#logout'
+
   root 'application#index'
 end
