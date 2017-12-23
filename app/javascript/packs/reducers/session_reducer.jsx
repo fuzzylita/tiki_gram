c0b68db1168d6f28a40889f663fbb079acd46344
Lita Stephenson <Lita.Stephenson@gmail.com>
2017-12-23 14:36:41 -0800

    bugfix: minor errors

--- app/javascript/packs/reducers/session_reducer.jsx
+++ app/javascript/packs/reducers/session_reducer.jsx
@@ -3,9 +3,11 @@ function sessionReducer(state, action){
   switch (action.type) {
     case 'LOGIN': // this case should receive a payload containing the user data (action.user_info)
       return {is_logged_in: true, user_info: action.user_info}
-    case 'LOGOUT' // this case should set the state to null or whatever
+    case 'LOGOUT': // this case should set the state to null or whatever
       return {is_logged_in: false, user_info: null}
     default:
       return state;
   }
 }
+
+export default sessionReducer
