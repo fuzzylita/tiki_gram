33982f0894b3fd4e58baf59121ac81a9d47d2dfa
Lita Stephenson <Lita.Stephenson@gmail.com>
2017-12-23 20:09:13 -0800

    renamed application.jsx to index.jsx

---
+++ app/javascript/packs/index.jsx
@@ -0,0 +1,30 @@
+/* eslint no-console:0 */
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+//
+// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
+// layout file, like app/views/layouts/application.html.erb
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux'; // we will use these when we encorporate redux
+import { Provider } from 'react-redux';
+
+import sessionReducer from './reducers/session_reducer'
+import App from './containers/app'
+
+const store = createStore(
+  sessionReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <App store={store}/>
+    </Provider>,
+    document.getElementById('app')
+  )
+})
