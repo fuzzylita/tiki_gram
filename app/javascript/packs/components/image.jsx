f74b0413861b128196851421cb339363327dc632
Lita Stephenson <Lita.Stephenson@gmail.com>
2017-12-25 19:35:06 -0800

    updated image component to a link component

--- app/javascript/packs/components/image.jsx
+++ app/javascript/packs/components/image.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Image = (props) => {
-
   return(
     <div>
-      <img src={props.image_data.images.low_resolution.url} />
+      <Link to={`/image/${props.photo.id}`} >
+        <img src={props.photo.src} />
+      </Link>
     </div>
   )
 }
