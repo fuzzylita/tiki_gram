b6099fff17cf2d2d4cbcc23c317cb9870753735d
Lita Stephenson <Lita.Stephenson@gmail.com>
2017-12-26 16:28:26 -0800

    updated favorites controller

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,15 +1,19 @@
 class FavoritesController < ApplicationController
-
+  protect_from_forgery except: [:create, :destroy]
+  
   def create
-    # TODO consider if this is the best key
-    favorites = Favorite.find_or_create_by(id: params[:id]) do |fav|
+    favorite = Favorite.find_or_create_by(image_id: params[:image_id], current_user.id) do |fav|
       fav.user_id = current_user.id
       fav.image_id = params[:image_id]
       fav.tags = params[:tags]
       fav.images = params[:images]
       fav.created_at = params[:created_at]
     end
-    render json: {message: "OK"}
+    if favorite.save
+      render json: {message: "OK"}
+    else
+      render json: {message: "Borked"}, status: :internal_server_error
+    end
   end
 
   def index
@@ -24,9 +28,8 @@ class FavoritesController < ApplicationController
     render json: favs
   end
 
-  def delete
-    # TODO validate user has permissions to delete favorite
-    favorite = Favorite.find_or_create_by(id: params[:id])
+  def destroy
+    favorite = Favorite.find_or_create_by(image_id: params[:image_id], current_user.id)
     favorite.delete
     render json: {message: "OK"}
   end
